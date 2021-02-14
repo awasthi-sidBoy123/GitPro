b461c0707aea32e73058b3cb8bc21c835630e775
Siddharth Awasthi <siddharth.a@macrew.net>
2021-02-14 16:39:11 +0530

    changes made by gitpro mac

--- src/test/java/SidProjects/MavenJavaData/dataMaven.java
+++ src/test/java/SidProjects/MavenJavaData/dataMaven.java
@@ -8,10 +8,13 @@ public class dataMaven {
 	@Test
 	public void mavDat() {
 		System.out.println("This is dat Mav ");
+		System.out.println("Welcome to gitpro");
 	}
 	
 	@BeforeTest
 	public void maveDat() {
 		System.out.println("This is before dat Mav ");
+		System.out.println("We welcome you to gitpro data");
+		System.out.println("We welcome you to gitpro data");
 	}
 }
