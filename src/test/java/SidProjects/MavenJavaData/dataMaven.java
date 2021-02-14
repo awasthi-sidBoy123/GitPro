c47b0c8368eafa0010d3fa74778ae38e40dc967e
GitPro <awasthisidboy123@gmail.com>
2021-02-15 00:08:17 +0530

    Commit after changig branch

--- src/test/java/SidProjects/MavenJavaData/dataMaven.java
+++ src/test/java/SidProjects/MavenJavaData/dataMaven.java
@@ -15,6 +15,26 @@ public class dataMaven {
 		System.out.println("Changes made by user1 and then pushed on git. User 2 plz check and approve. Ok I am approving it.User2 ");
 	}
 	
+
+	@Test
+	public void maviDat() {
+		System.out.println("This is dat Mav ");
+		System.out.println("Welcome to gitpro");
+		System.out.println("Changes made by user1");
+		System.out.println("Changes made by user1");
+		System.out.println("Changes made by user1 and then pushed on git. User 2 plz check and approve.");
+		System.out.println("Changes made by user1 and then pushed on git. User 2 plz check and approve. Ok I am approving it.User2 ");
+	
+	}
+	
+	@Test
+	public void mavsiDat() {
+		System.out.println("This is dat Mavsi ");
+		System.out.println("Welcome to gitproi");
+		System.out.println("Changes made by user1");
+		System.out.println("Changes made by user1");
+	
+	
 	@BeforeTest
 	public void maveDat() {
 		System.out.println("This is before dat Mav ");
